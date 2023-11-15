933276ab6ff623a93acd8315275bfc19995ff637
TTONMAII <tonmai.w@gmail.com>
2023-11-16 04:17:29 +0700

    midtermproject

--- src/Info.jsx
+++ src/Info.jsx
@@ -11,7 +11,7 @@ export const InfoComponent = () => {
 <Carousel showThumbs={false} autoPlay={true} stopOnHover={false} infiniteLoop={true} interval={3000}>
     { Array.from(Array(15).keys()).map((element,index)=>{
         return <div>
-        <img src= {`./${index+1}.jpg`} />
+        <img src= {`./${index+1}.jpg`} alt="error" />
         { index === 0 &&<div className="item1">
             Warakorn Konsila <br />6304186
         </div>}
