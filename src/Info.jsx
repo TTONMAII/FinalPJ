fb481faf5a7bc22c7bea3452c53971b45759fe58
TTONMAII <150965590+TTONMAII@users.noreply.github.com>
2023-11-17 15:59:19 +0700

    Update Info.jsx

--- src/Info.jsx
+++ src/Info.jsx
@@ -3,10 +3,9 @@ import { Carousel } from 'react-responsive-carousel';
 
 export const InfoComponent = () => {
     return <div>
-       {/* <img src={img1} />
- <div className="item1">
-   Warakorn Konsila <br />6304186
- </div> */}
+       {
+           
+       }
 
 <Carousel showThumbs={false} autoPlay={true} stopOnHover={false} infiniteLoop={true} interval={3000}>
     { Array.from(Array(15).keys()).map((element,index)=>{
