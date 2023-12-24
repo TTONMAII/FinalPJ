e3fd07c7a993a639678a04aebb9a4b822c37010b
TTONMAII <tonmai.w@gmail.com>
2023-12-25 03:36:58 +0700

    update pic

--- src/profile.jsx
+++ src/profile.jsx
@@ -2,7 +2,7 @@ function Profile() {
   return (
     <div>
       <div style={{padding:"20px 0"}}> About ME</div>
-        <img style={{width:"300px",height:"300px", borderRadius:"100%"}} src={"Profile.jpg"} alt=""/>
+        <img style={{width:"300px",height:"300px", borderRadius:"100%"}} src={"profile.jpg"} alt=""/>
         <div>Name : Warakorn Konsila</div>
         <div>Age : 23 </div>
         <div>Birthday : 29/04/2000</div>
